316e425b66a68a1c344a75227dadd0aa629d3a99
Moises H <moises.h@outlook.com>
2023-06-15 18:19:49 -0400

    Bug

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-export { default as Navbar } from "./Navbar/navbar";
-export { default as Footer } from "./Footer/footer";
+export { default as Navbar } from "./Navbar/navbar.js";
+export { default as Footer } from "./Footer/footer.js";
 const placeholder = {};
 export default placeholder;
