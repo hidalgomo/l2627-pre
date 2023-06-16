66811f3a2a2157efb2c68d1b3fcb226884797101
Moises H <moises.h@outlook.com>
2023-06-16 12:37:45 -0400

    Minor Fixes to title panel

--- src/components/title-panel.js
+++ src/components/title-panel.js
@@ -40,7 +40,7 @@ export default function TitlePanel(props) {
                                     <div className="mt-6 border-t border-gray-100">
                                         <dl className="divide-y divide-gray-100">
                                             <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                                                <dt className="text-sm font-medium leading-6 text-gray-900">Full name</dt>
+                                                <dt className="text-sm font-medium leading-6 text-gray-900">Title name</dt>
                                                 <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{ props.title.name }</dd>
                                             </div>
                                             <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
@@ -52,7 +52,7 @@ export default function TitlePanel(props) {
                                                 <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{ props.title.scope }</dd>
                                             </div>
                                             <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                                                <dt className="text-sm font-medium leading-6 text-gray-900">Level & Rate</dt>
+                                                <dt className="text-sm font-medium leading-6 text-gray-900">Level & rate</dt>
                                                 <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">$120,000</dd>
                                             </div>
                                             <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
@@ -63,11 +63,13 @@ export default function TitlePanel(props) {
                                                             <div className="flex w-0 flex-1 items-center">
                                                                 <PaperClipIcon className="h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
                                                                 <div className="ml-4 flex min-w-0 flex-1 gap-2">
-                                                                    <span className="truncate font-medium">resume_back_end_developer.pdf</span>
+                                                                    <span className="truncate font-medium">title_specifications.pdf</span>
                                                                 </div>
                                                             </div>
                                                             <div className="ml-4 flex-shrink-0">
-                                                                <Link href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                                                                <Link to={ 'https://local2627.org' + props.title.doc_link } target='_blank' 
+                                                                    className="font-medium text-indigo-600 hover:text-indigo-500">
+                                                                    
                                                                     Download
                                                                 </Link>
                                                             </div>
