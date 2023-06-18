0d22c880dd739b158c037cbda73124867385d66c
Arlyn Acevedo Vidal <arlynacevedo@yahoo.com>
2023-06-18 13:16:57 -0400

    #22 add benefits, grievances, and telework views

--- src/app-routes.js
+++ src/app-routes.js
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import { Navbar, Footer } from './components';
-import { Education, Home, Leadership, Titles } from './views';
+import { Benefits, Education, Grievances, Home, Leadership, Telework, Titles } from './views';
 
 const AppRoutes = () => {
     return (
@@ -8,10 +8,13 @@ const AppRoutes = () => {
             <Navbar />
             <div className="container mx-auto max-w-full p-4 sm:p-6 lg:p-8">
                 <Routes>
-                    <Route path="education" element={ <Education /> } />
-                    <Route path="/" element={ <Home /> } />
-                    <Route path="leadership" element={ <Leadership /> } />
-                    <Route path="titles" element={ <Titles /> } />
+                    <Route path="education" element={<Education />} />
+                    <Route path="/" element={<Home />} />
+                    <Route path="leadership" element={<Leadership />} />
+                    <Route path="titles" element={<Titles />} />
+                    <Route path="grievances" element={<Grievances />} />
+                    <Route path="benefits" element={<Benefits />} />
+                    <Route path="telework" element={<Telework />} />
                 </Routes>
             </div>
             <Footer />
---
+++ src/views/telework.js
@@ -0,0 +1,84 @@
+export default function Telework() {
+    const teleworkContainerStyle = {
+        padding: "20px 30px"
+    };
+
+    return <section class="container-fluid" style={teleworkContainerStyle}>
+        <h3 class="header-label">Telework</h3>
+        <h4>Updated Guidance Related to Attendance, Leave, and Timekeeping Policies During COVID-19</h4>
+        <ul>
+            <li id="Personnel_Memorandum_No_1-2022-23">
+                <a href="/resources/pdf/Personnel_Memorandum_No_1-2022-23.pdf" target="_blank" title="Personnel Memorandum No 1-2022-23">
+                    COVID-19 Personnel Memorandum No 1-2022-23
+                </a>
+            </li>
+        </ul>
+        <h4>Updated Leave Guidance Applicable to Employees During Outbreak</h4>
+        <p>
+            Updated Guidance for City Agencies on Leave Policy Applicable During the Outbreak of Coronavirus Disease
+            2019 (COVID-19), eff. March 30, 2022
+        </p>
+        <p>
+            This document sets forth City leave policy with respect to City employees to mitigate the risk of the
+            spread of Coronavirus Disease 2019 (COVID-19). It is effective March 30, 2022 and until further notice, and
+            supersedes the January 26, 2022 guidance.
+            <a href="/resources/pdf/Updated_Leave_Guidance_Applicable_to_Employees_During_Outbreak_20220330.pdf" target="_blank" title="Updated Leave Guidance Applicable to Employees During Outbreak">
+                Read more
+            </a>
+        </p>
+        <h4>Telework Policy for Mayoral Agencies</h4>
+        <p>
+            <a href="/resources/pdf/PSB_600_3_Temporary_Citywide_Telework_Policy_for_City_Employees_During_t.pdf" target="_blank" title="PSB 600.3 Temporary Citywide Telework Policy for City Employees During the current COVID-19 Outbreak">
+                PSB 600.3 Temporary Citywide Telework Policy for City Employees During the current COVID-19 Outbreak
+            </a>
+        </p>
+        <h4>H+H Telework Policy</h4>
+        <p>
+            <a href="/resources/pdf/COVID19_Hospitals_Teleworking.pdf" target="_blank" title="COVID-19 NYC Health + Hospitals Telework Policy">COVID-19
+                NYC Health + Hospitals Telework Policy
+            </a>
+        </p>
+        <h4>Remote Work Guidelines for CUNY</h4>
+        <p>
+            <a href="/resources/pdf/CUNY_Remote_Work_Guidelines.pdf" target="_blank" title="CUNY Remote Work Guidelines">
+                CUNY Remote Work Guidelines
+            </a>
+        </p>
+        <h4>Emergency Childcare</h4>
+        <p>
+            <a href="/resources/pdf/Regional_Enrichment_Centers_CONVID19.pdf" target="_blank" title="Regional Enrichment Centers (CONVID-19)">
+                Regional Enrichment Centers
+            </a>
+        </p>
+        <p>
+            <a href="/resources/pdf/MTA_Bulletin_on_Child_Care_CONVID19.pdf" target="_blank" title="MTA Bulletin on Child Care (CONVID-19)">
+                MTA Bulletin on Child Care
+            </a>
+        </p>
+        <h3 class="header-label">Fund the Front Lines</h3>
+        <p>
+            This isn't about partisanship. It's about clean water, road repairs, safe schools, first responders and
+            well-staffed hospitals. And the bottom line is this: we can't defeat the coronavirus and reopen the economy
+            without strong, well-funded public services. We're shouting this message from the rooftops — and now over
+            the airwaves.
+        </p>
+        <p>Contact Congress and tell them to fund the front lines by texting FUND to 237-263.</p>
+        <p>
+            <iframe title="Public Service Army" height="315" width="560" border="0" class="boxes" src="https://www.youtube-nocookie.com/embed/NvpBk400Ifw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="allowfullscreen">
+            </iframe>
+        </p>
+        <p>
+            <iframe title="Front-Line Fighters" height="315" width="560" border="0" class="boxes" src="https://www.youtube-nocookie.com/embed/gOb0FHT4xsI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="allowfullscreen">
+            </iframe>
+        </p>
+        <ul>
+            <li><a href="/covid19" title="COVID-19">COVID-19</a></li>
+            <li>
+                <a href="/resources/pdf/PSB_600_4_Temporary_Citywide_Telework_Policy_for_City_Employees_During_t.pdf" target="_blank" title="PSB 600.4 Temporary Citywide Telework Policy for City Employees During the current COVID-19 Outbreak">
+                    Telework for Mayoral Agencies Telework Policy
+                </a>
+            </li>
+            <li>CSM's Accretion</li>
+        </ul>
+    </section>
+}
