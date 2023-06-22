7878fdd232e0ae1b4df2e616159c6434c2e6fc8d
Moises H <moises.h@outlook.com>
2023-06-22 11:42:21 -0400

    Tailwindcss added to Grievance view

--- src/views/grievances.js
+++ src/views/grievances.js
@@ -1,122 +1,127 @@
 export default function Grievances() {
-    const grievanceContainerStyle = {
-        maxWidth: "700px"
-    };
+    return (
+        <div className="max-w-3xl">
+            <h3 className="text-3xl font-bold pb-2">Grievance</h3>
 
-    return <div style={grievanceContainerStyle}>
-        <section>
-            <h3>How to File a Grievance</h3>
-            <p>
-                When, how and why to file a grievance
+            <div>
+                <p>
+                    When, how and why to file a grievance
+                    <br />
+                    Your union contract provides a
+                    mechanism for you to make sure your employer abides by
+                    the contract. That mechanism is the grievance procedure.
+                    When an employer violates a provision of the contract,
+                    union members have a right to file a grievance.
+                </p>
+            </div>
+
+            <h3 className="text-2xl py-4">When to File a Grievance</h3>
+            <div>
+                <p>
+                    File a grievance whenever
+                    your employer violates a provision of the contract. If
+                    you are not sure about what the contract says on the
+                    matter, talk it over with your shop steward. Some
+                    examples of typical grievance issues are out-of-title
+                    work, overtime pay, and disciplinary procedures.
+                </p>
                 <br />
-                Your union contract provides a
-                mechanism for you to make sure your employer abides by
-                the contract. That mechanism is the grievance procedure.
-                When an employer violates a provision of the contract,
-                union members have a right to file a grievance.
-            </p>
-        </section>
+                <p>
+                    Not every gripe is a
+                    grievance there are problems you may face that are not
+                    covered in the contract (for instance, your boss may be
+                    rude). Grievances are specifically about contract
+                    violations, but sometimes are more broadly defined. But
+                    even if your complaint doesn't meet the definition of a
+                    grievance, it doesn't mean you can't take action you,
+                    your steward, other workers in your shop and the union
+                    together can organize and pressure the employer.
+                </p>
+                <br />
+                <p>
+                    On the other hand, if the
+                    problem you are having is covered in the contract, then
+                    you should definitely file a grievance.
+                </p>
+            </div>
 
-        <section>
-            <h4>When to File a Grievance</h4>
-            <p>
-                File a grievance whenever
-                your employer violates a provision of the contract. If
-                you are not sure about what the contract says on the
-                matter, talk it over with your shop steward. Some
-                examples of typical grievance issues are out-of-title
-                work, overtime pay, and disciplinary procedures.
-            </p>
-            <p>
-                Not every gripe is a
-                grievance there are problems you may face that are not
-                covered in the contract (for instance, your boss may be
-                rude). Grievances are specifically about contract
-                violations, but sometimes are more broadly defined. But
-                even if your complaint doesn't meet the definition of a
-                grievance, it doesn't mean you can't take action you,
-                your steward, other workers in your shop and the union
-                together can organize and pressure the employer.
-            </p>
-            <p>
-                On the other hand, if the
-                problem you are having is covered in the contract, then
-                you should definitely file a grievance.
-            </p>
-        </section>
+            <h3 className="text-2xl py-4">How a Grievance works</h3>
+            <div>
+                <p>
+                    Shop stewards have copies of
+                    grievance forms. The first step in pursuing a grievance
+                    is to talk to your steward and fill out the form. The
+                    grievance should contain a clear statement of the issue
+                    and a reference to the relevant rule or contract
+                    section.
+                </p>
+                <br />
+                <p>
+                    Local 2627 members work in
+                    mayoral and non-mayoral agencies. The grievance procedure
+                    for each contract is a little different. However, they
+                    all follow a general pattern that includes three or four
+                    steps.
+                </p>
+                <br />
+                <p>
+                    THE INITIAL STEP is to take the
+                    written account of the grievance (the grievance form) to
+                    the immediate supervisor or department head. A grievance
+                    must be initiated within a certain number of days from
+                    when the event occurred that is being grieved; check your
+                    contract to make sure you know the deadlines that apply
+                    in your case. This step involves the member, the steward
+                    and immediate management. Management has a certain number
+                    of days to respond to the grievance. If they do not
+                    respond in that time frame, or if the member or the union
+                    is not satisfied with the response at this level, the
+                    grievance proceeds to the next step.
+                </p>
+                <br />
+                <p>
+                    THE NEXT STEP or steps involves
+                    taking the case to higher levels of management. At this
+                    step, the staff rep usually gets involved. A hearing may
+                    be held, and witnesses may be called. Again, there is a
+                    specific number of days in which management must respond.
+                    If the member or the union is not satisfied, they may
+                    appeal the decision to the next step. Here, too, you must
+                    act within a certain number of days.
+                </p>
+                <br />
+                <p>
+                    THE FINAL STEP in a grievance
+                    procedure is binding arbitration. Binding arbitration
+                    means that an impartial, outside party hears the case and
+                    makes a decision, which the employer, the worker and the
+                    union must obey. As part of the contract, the union and
+                    the employer have agreed in advance on individuals who
+                    will serve as arbiters.
+                </p>
+            </div>
 
-        <section>
-            <h4>How a Grievance works</h4>
-            <p>
-                Shop stewards have copies of
-                grievance forms. The first step in pursuing a grievance
-                is to talk to your steward and fill out the form. The
-                grievance should contain a clear statement of the issue
-                and a reference to the relevant rule or contract
-                section.
-            </p>
-            <p>
-                Local 2627 members work in
-                mayoral and non-mayoral agencies. The grievance procedure
-                for each contract is a little different. However, they
-                all follow a general pattern that includes three or four
-                steps.
-            </p>
-            <p>
-                THE INITIAL STEP is to take the
-                written account of the grievance (the grievance form) to
-                the immediate supervisor or department head. A grievance
-                must be initiated within a certain number of days from
-                when the event occurred that is being grieved; check your
-                contract to make sure you know the deadlines that apply
-                in your case. This step involves the member, the steward
-                and immediate management. Management has a certain number
-                of days to respond to the grievance. If they do not
-                respond in that time frame, or if the member or the union
-                is not satisfied with the response at this level, the
-                grievance proceeds to the next step.
-            </p>
-            <p>
-                THE NEXT STEP or steps involves
-                taking the case to higher levels of management. At this
-                step, the staff rep usually gets involved. A hearing may
-                be held, and witnesses may be called. Again, there is a
-                specific number of days in which management must respond.
-                If the member or the union is not satisfied, they may
-                appeal the decision to the next step. Here, too, you must
-                act within a certain number of days.
-            </p>
-            <p>
-                THE FINAL STEP in a grievance
-                procedure is binding arbitration. Binding arbitration
-                means that an impartial, outside party hears the case and
-                makes a decision, which the employer, the worker and the
-                union must obey. As part of the contract, the union and
-                the employer have agreed in advance on individuals who
-                will serve as arbiters.
-            </p>
-        </section>
-
-        <section>
-            <h4>Why Filing a Grievance is Important</h4>
-            <p>
-                Sometimes union members know
-                the employer is violating the contract but they are
-                hesitant to file a grievance because they think it's no
-                big deal or they don't want to get in trouble. But
-                contract violations are a big deal! And it's your right
-                to grieve them; your boss cannot retaliate against you
-                for filing a grievance. Whenever we let an employer
-                violate the contract without challenging him, we are
-                weakening the contract for all Local 2627 members it
-                sends a message that we will not enforce the contract.
-                When we file grievances, we let employers know that we
-                take the contract seriously and we will make them take it
-                seriously, too. Sometimes, just knowing union members
-                will enforce their rights acts as a deterrent, and
-                management will think twice before violating the
-                contract.
-            </p>
-        </section>
-    </div>
+            <h3 className="text-2xl py-4">Why Filing a Grievance is Important</h3>
+            <div>
+                <p>
+                    Sometimes union members know
+                    the employer is violating the contract but they are
+                    hesitant to file a grievance because they think it's no
+                    big deal or they don't want to get in trouble. But
+                    contract violations are a big deal! And it's your right
+                    to grieve them; your boss cannot retaliate against you
+                    for filing a grievance. Whenever we let an employer
+                    violate the contract without challenging him, we are
+                    weakening the contract for all Local 2627 members it
+                    sends a message that we will not enforce the contract.
+                    When we file grievances, we let employers know that we
+                    take the contract seriously and we will make them take it
+                    seriously, too. Sometimes, just knowing union members
+                    will enforce their rights acts as a deterrent, and
+                    management will think twice before violating the
+                    contract.
+                </p>
+            </div>
+        </div>
+    );
 }
