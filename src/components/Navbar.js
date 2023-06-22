fff0a3488d2a774094e2bf6624bb30678b26dcba
Moises H <moises.h@outlook.com>
2023-06-21 20:17:07 -0400

    Sticky Top Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -67,7 +67,7 @@ export default function Navbar() {
     };
 
     return (
-        <div className="bg-white">
+        <div className="bg-white sticky top-0">
             {/* Mobile menu */}
             <Transition.Root show={ open } as={ Fragment }>
                 <Dialog as="div" className="relative z-40 lg:hidden" onClose={ setOpen }>
