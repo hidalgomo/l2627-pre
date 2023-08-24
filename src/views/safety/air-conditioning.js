f338acc01c2a857f6474560bc080f978629bec0c
Moises H <moises.h@outlook.com>
2023-08-24 18:26:56 -0400

    Air conditioning content added

--- src/views/safety/air-conditioning.js
+++ src/views/safety/air-conditioning.js
@@ -2,6 +2,50 @@ export function AirConditioning() {
     return (
         <div className="max-w-3xl">
             <h3 className="text-3xl font-bold pb-8">Air Conditioning</h3>
+            
+            <p className="text-xl font-bold mb-2">Interpretive Memorandum No. 52</p>
+            <ul className="list-disc mb-4 pl-8">
+                <li className="mb-2">
+                    Article V, Section 18 of the 1978-80 City-wide Agreement, abolishes shortened work-day schedules and 
+                    heat days in lieu thereof for employees who work in air conditioned facilities.
+                </li>
+                <li className="mb-2">
+                    Where there is a complete breakdown of air conditioning, the following policy shall be applicable to 
+                    employees who have traditionally enjoyed such summer schedules:
+
+                    <p className="p-6">
+                        There there is complete breakdown of air conditioning, the City will have one day from the date 
+                        of notification by the Union to repair the air conditioning system. If the air conditioning 
+                        has not been repaired after the first day, affected employees shall receive, in the discretion 
+                        of the agency, beginning with the second day of the complete breakdown, either compensatory 
+                        time credit of one hour or the shortened work day schedule for each remaining day during the 
+                        breakdown. This policy shall apply City-wide.
+                    </p>
+                </li>
+                <li className="mb-2">
+                    When there is an allegation of a partial breakdown of air conditioning, a designated employer 
+                    representative shall take the temperature in the center of the air conditioned area 
+                    approximately 1 1/2 hours prior to the end of the workday. A partial breakdown of air conditioning 
+                    for the purposes of this Interpretive Memorandum shall occur when the employer representative 
+                    certifies that despite the use of the air conditioning equipment the temperature indoors 
+                    exceeds 85°. The employer representative may be accompanied by one employee representative 
+                    for the purpose of observing the temperature reading.
+                </li>
+                <li className="mb-2">
+                    Where there is such a partial breakdown of air conditioning, as certified by the employer 
+                    representative, the following policy shall be applicable to employees who have traditionally 
+                    enjoyed summer schedules:
+
+                    <p className="p-6">
+                        The employer will have one day from the date of notification by the Union to repair the air 
+                        conditioning. If the air conditioning has not been repaired after the first day, eligible 
+                        affected employees shall receive in the discretion of the agency, either compensatory time 
+                        credit of one hour, or the shortened work-day schedule for each remaining day worked which 
+                        exceeds the above guidelines.
+                    </p>
+                </li>
+                <li className="mb-2">NOTE: A CYCLOMETER (SWING THERMOMETER) IS A USEFUL METHOD FOR QUICK MEASUREMENT OF ROOM TEMPERATURE.</li>
+            </ul>
         </div>
     );
 }
