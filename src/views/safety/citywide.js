8cd5b8934b1d4f2c418ead4776eafe9ed460ce8d
Moises H <moises.h@outlook.com>
2023-08-24 17:20:33 -0400

    citywide safety content added

--- src/views/safety/citywide.js
+++ src/views/safety/citywide.js
@@ -1,9 +1,228 @@
 export function Citywide() {
     return (
         <div className="max-w-3xl">
-            <h3 className="text-3xl font-bold pb-8">Citywide Safety</h3>
+            <h3 className="text-3xl font-bold pb-8">Citywide Safety Contract</h3>
 
+            <p className="text-xl font-bold mb-2">ARTICLE XIV — OCCUPATIONAL SAFETY AND HEALTH</p>
+            <p className="font-bold mb-2">Section 1</p>
+            <p className="mb-4">
+                The Employer shall establish a Citywide Occupational Safety and Health Committee, the members 
+                of which shall be appointed by the Mayor and shall include union representation. The Director 
+                of the Citywide Office on Safety and Health shall serve, ex officio, as Chairperson of this Committee.
+            </p>
+            <p className="mb-6">
+                The Citywide Occupational Safety and Health Committee shall recommend citywide employee safety and 
+                health policy to the Mayor and shall assume the duties and responsibilities of the Occupational Safety 
+                and Health Planning Task Force created by Mayor's Executive Order No. 58, dated May 6, 1976, and 
+                shall also assume the citywide safety responsibilities of the City Director of Personnel contained in 
+                Mayor's Executive Order No. 109, dated August 28, 1969. In addition, this Committee shall act as the 
+                City's liaison with Federal and State Agencies, in efforts to obtain grants to finance City employee 
+                safety and health programs and shall perform any additional tasks assigned by the Mayor.
+            </p>
+                
+            <p className="font-bold mb-2">Section 2</p>
+            <ol className="pl-8 mb-6" style={{ listStyleType: 'lower-alpha' }}>
+                <li className="mb-2">
+                    Adequate, clean, structurally safe and sanitary working facilities shall be provided 
+                    for all employees.
+                    </li>
+                <li className="mb-2">
+                    Motor vehicles and power equipment which are in compliance with minimum standards of 
+                    applicable law shall be provided to employees who are required to use such devices.
+                </li>
+                <li className="mb-2">
+                    Where necessary, first aid chests, adequately marked and stocked, shall be provided by the Employer 
+                    in sufficient quantity for the number of employees likely to need them and such chests shall be 
+                    reasonably accessible to the employees.
+                </li>
+                <li className="mb-2">
+                    A Labor Management Health and Safety Committee shall be established in each agency. Each committee 
+                    shall be composed of not less than three nor more than five labor representatives designated by the 
+                    Union and not more than an equivalent number of management representatives designated by the agency. 
+                    The appropriate number of such representatives shall be determined jointly. If agreement on 
+                    the number cannot be reached such number shall be determined by the Commissioner of Labor 
+                    Relations. The Committee shall meet at least quarterly and shall meet at the written request 
+                    of the labor or the management representatives for the purpose of discussing health and 
+                    safety problems in the agency and making recommendations for their resolution to the agency 
+                    head. The written request for such a meeting shall indicate the specific condition for 
+                    which the meeting is called. In addition to the above described committee, sub-committees 
+                    may be established on an ad hoc basis upon agreement of the parties.
+                </li>
+                <li className="mb-2">
+                    The sole remedy for alleged violations of this section shall be a grievance pursuant to Article XV 
+                    of this Agreement. Any employee who withholds services as a means of redressing or otherwise protesting 
+                    alleged violations of this section shall be docked pay for any unauthorized non-performance of work and 
+                    may be subject to any appropriate disciplinary action.    
+                </li>
+                <li className="mb-2">
+                    In construing this section, an arbitrator shall initially have the power only to decide whether the 
+                    subject facilities meet the standards of subsection (a) of this section 2 but may not affirmatively 
+                    direct how the Employer should comply with this section. If the arbitrator determines that the 
+                    Employer is in violation of this section, the Employer shall take appropriate steps to remedy 
+                    the violation. If in the opinion of the Union the Employer does not achieve compliance within 
+                    a reasonable period of time, the Union may reassert its claim to the arbitrator. Upon such 
+                    second submission, if the arbitrator finds that the Employer has had a reasonable time to 
+                    comply with the terms of this section and has failed to do so, then and only then, the 
+                    arbitrator may order the Employer to follow a particular course of action which will 
+                    effectuate compliance with the terms of this section. However, such remedy shall not 
+                    exceed appropriations available in the current budget allocation for the involved agency 
+                    for such purposes.
+                </li>
+                <li className="mb-2">
+                    In any enclosed facility where employees are assigned to work, the Employer shall make reasonable 
+                    efforts to provide for the personal security of employees while they are working.
+                </li>
+                <li className="mb-2">
+                    When the Employer becomes aware of a safety hazard which the Employer considers an imminent 
+                    physical danger to employees at a worksite, the Employer shall remove the employees from the 
+                    affected area.
+                </li>
+                <li>
+                    The Employer shall provide to the Municipal Labor Committee a copy of the results of environmental 
+                    testing by the City of a City worksite and statistics resulting from special medical testing of 
+                    employees.
+                </li>
+            </ol>
 
+            <p className="text-xl font-bold mb-2">ARTICLE XVIII — VDT OPERATORS</p>
+
+            <p className="font-bold mb-2">Section 1. Applicability</p>
+            <p className="mb-4">
+                Except as otherwise specifically indicated in this Article XVIII, the terms "employee" and "employees 
+                shall mean only a full-time worker who regularly and for continuous periods of time operate VDT 
+                terminals 20 hours or more per week."
+            </p>
+
+            <p className="font-bold mb-2">Section 2. Alternative Work Break</p>
+            <p className="mb-4">
+                Employees covered by this Article shall not be required to continuously operate a VDT terminal 
+                for more than two (2) consecutive hours without an assignment to alternative work of a visually 
+                less demanding nature for a period of not less than fifteen (15) minutes. Meal periods and any 
+                previously established rest periods shall count towards meeting the requirement for alternative 
+                work, but this provision shall not be construed as providing any additional non-work break 
+                time. The provisions of this section shall also apply to part time employees subject to the terms 
+                of the Citywide Agreement who regularly and for continuous periods of time operate VDT terminals 
+                20 hours or more per week.
+            </p>
+
+            <p className="font-bold mb-2">Section 3. Alternative Work</p>
+            <ol className="pl-8 mb-6" style={{ listStyleType: 'lower-alpha' }}>
+                <li className="mb-2">
+                    Upon submission of proof satisfactory to the agency head or the agency head's designee that an 
+                    employee covered by this Article is physically incapable from operating a VDT terminal due to 
+                    injury, disability, or pregnancy, the Employer shall make every effort to assign such 
+                    employee to appropriate, alternative duties in the same title for the period of such disability, 
+                    provided that such temporary assignments shall not be required to exceed one year. If a suitable 
+                    position is not available, the Employer shall offer the employee any available opportunity to 
+                    transfer to another title for which the employee may qualify by the change of title procedure 
+                    followed by the New York City Department of Citywide Administrative Services pursuant to 
+                    Rule 6.1.1. of the Personnel Rules and Regulations of the City of New York or by non-competitive 
+                    examination offered pursuant to Rule 6.1.9. of the Personnel Rules and Regulations of the 
+                    City of New York or Rule 4.1.8 of the Health and Hospitals Corporation Personnel Rules and 
+                    Regulations.
+                </li>
+                <li className="mb-2">
+                    If such an employee has ten (10) or more years of retirement system membership service and is 
+                    considered permanently unable to perform all the duties of the employee's title and no 
+                    suitable in-title position is available, the employee shall be referred to the New York 
+                    City Employee's Retirement System and recommended for ordinary disability retirement.
+                </li>
+            </ol>
+
+            <p className="font-bold mb-2">Section 4. Training</p>
+            <p className="mb-6">
+                The Citywide Office of Occupational Safety and Health, and the Union shall jointly develop a module 
+                on VDT operational safety for inclusion in agency orientational training programs. In the Health 
+                and Hospitals Corporation, such training module shall be developed by Human Resources (in 
+                conjunction with the Corporate Office of Occupational and Environmental Health Services) and the 
+                Union. Such training module shall also be offered to current employees as part of any regular 
+                Right to Know Training given in the normal course of business. The provisions of this section 
+                4 shall apply to all employees subject to the terms of the Citywide Agreement regardless of 
+                the number of hours of employment.
+            </p>
+
+            <p className="font-bold mb-2">Section 5. VDT and Related Equipment</p>
+            <ol className="pl-8 mb-6" style={{ listStyleType: 'lower-alpha' }}>
+                <li classname="mb-2">
+                    A standing VDT Sub-Committee of the Citywide Occupational Safety and Health Committee instituted 
+                    pursuant to Article XIV, section 1 of this Agreement, shall be established with joint Employer and 
+                    Union membership. Such sub-committee shall study, issue, and periodically review procurement and 
+                    ergonomic standards for VDT's and ancillary furniture and equipment. The joint sub-committee's 
+                    initial report and recommendations shall be issued within three months of the execution of this 
+                    Agreement. A separate labor-management committee shall be established in the Health and Hospitals 
+                    Corporation for such purposes.
+                </li>
+                <li classname="mb-2">
+                    Procurement and ergonomic standards shall be implemented by Mayoral directive. In the Health and 
+                    Hospitals Corporation such standards shall be implemented in accordance with existing 
+                    procedures. Agencies and facilities of the Health and Hospitals Corporation shall purchase new 
+                    equipment and ancillary furniture and equipment in compliance with the then current Mayoral directive 
+                    or Health and Hospital Corporation procedure.
+                </li>
+                <li classname="mb-2">
+                    Agencies shall advise the Union of the installation and proposed utilization of new VDT equipment, 
+                    and shall make service logs available on a reasonable basis to qualified, authorized Union personnel.
+                </li>
+                <li classname="mb-2">
+                    Employees may at any time informally discuss alleged violation(s) of this section with their 
+                    supervisors. The Union may also seek to resolve any alleged violation(s) of this section through 
+                    the agency Labor Management Health and Safety Committees.
+                </li>
+                <li classname="mb-2">
+                    If a complaint alleging violation(s) of this section cannot be resolved pursuant to 
+                    subsection 5(d), such complaint may be filed by the Union in writing with the Director of the 
+                    Citywide Office of Occupational Safety and Health or the Director's designee, or, in the Health 
+                    and Hospitals Corporation, the Director of Occupational Health and Environmental Services or 
+                    the Director's designee, who will investigate such alleged violation(s) and issue a 
+                    determination within forty-five (45) days of the receipt of the complaint. Upon determination 
+                    by the Director or the Director's designee that violation(s) has occurred, the affected agency 
+                    or agencies shall be notified of the nature of the violation(s) and directed to take steps to 
+                    correct the violation(s) within sixty (60) days. A complaint pursuant to the sub-section must 
+                    be brought within sixty (60) days of the initial occurrence of the alleged violation(s).
+                </li>
+                <li classname="mb-2">
+                    A dispute concerning a determination by the Director of the Office of Citywide Occupational 
+                    Health and Safety or the Director's designee, or, in the Health and Hospitals Corporation, 
+                    the Director of Occupational Health and Environmental Services or the Director's designee 
+                    may be appealed by the Union in writing to the Commissioner of Labor Relations within 
+                    ten (10) work days of its issuance for resolution pursuant to Article XV, section 11 of 
+                    this Agreement.
+                </li>
+                <li classname="mb-2">
+                    A complaint concerning failure by an agency or agencies to comply with a determination issued 
+                    by the Director of the Office of Citywide Occupational Health and Safety or the Director's 
+                    designee, or, in the Health and Hospitals Corporation, the Director of Occupational Health 
+                    and Environmental Services or the Director's designee may be filed with the Commissioner 
+                    of Labor Relations within ten (10) working days of the expiration of the time limits set 
+                    forth in sub-section 5(e) for resolution pursuant to Article XV, section 11 of this Agreement.
+                </li>
+                <li classname="mb-2">
+                    The provisions of this section 5 shall apply to all employees subject to the terms of the 
+                    Citywide Agreement regardless of the number of hours of employment.
+                </li>
+            </ol>
+
+            <p className="font-bold mb-2">Section 6. Eye Examinations and Corrective Lenses</p>
+            <ol className="pl-8 mb-6" style={{ listStyleType: 'lower-alpha' }}>
+                <li classname="mb-2">
+                    The parties shall form a joint sub-committee to develop a program related to the provision of 
+                    eye examinations and corrective lenses for VDT operators. It is understood that said sub-committee 
+                    shall be charged with developing a program which will operate in the most cost efficient 
+                    manner possible.
+                </li>
+                <li classname="mb-2">
+                    The guidelines under which the sub-committee shall study the issues are:
+                    <ol className="pl-8 mb-6" style={{ listStyleType: 'lower-roman' }}>
+                        <li classname="mb-2">The provision of a base line examination with a follow-up examination every second year.</li>
+                        <li classname="mb-2">The provision of corrective lenses if necessary due to the operation of a VDT terminal.</li>
+                        <li classname="mb-2">Establishment of a cap on the costs of providing the examinations and lenses.</li>
+                        <li classname="mb-2">
+                            Allowance by the Employer to covered employees of up to two (2) hours of time to take 
+                            the baseline examination and follow-up examinations.
+                        </li>
+                    </ol>
+                </li>
+            </ol>
         </div>
     );
 }
