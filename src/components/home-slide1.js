01f8072495f0bfef9b2d40451617dce5707e71e0
Moises H <moises.h@outlook.com>
2023-09-09 22:09:32 -0400

    font fix

--- src/components/home-slide1.js
+++ src/components/home-slide1.js
@@ -2,7 +2,7 @@ export function HomeSlide1() {
     return (
         <div className="sm:mx-6 lg:mx-8 py-24 sm:py-32 mx-auto max-w-7xl lg:px-8 sm:px-6 px-11">
             <div className="mx-auto max-w-2xl lg:mx-0">
-                <h2 className="lg:text-lg md:text-xl text-4xl mb-6 font-bold tracking-tight text-white">Welcome</h2>
+                <h2 className="lg:text-2lg md:text-2xl text-4xl mb-6 font-bold tracking-tight text-white">Welcome</h2>
                 <p className="text-lg leading-8 text-gray-300">
                     IT Professionals of the City of New York represents over <span className="font-bold">6,000</span> employees who work in IT 
                     titles across all City of New York agencies.
