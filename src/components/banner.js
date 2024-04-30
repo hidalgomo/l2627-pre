94c585d7e4d366f873b3eca88b51b943fb817655
Moi Hi <moises.h@outlook.com>
2024-04-30 10:25:12 -0400

    Date Fix for Meeting Registration

--- src/components/banner.js
+++ src/components/banner.js
@@ -5,7 +5,7 @@ export function Banner() {
         <div className="bg-red-300 lg:px-8 sm:px-6 px-11 py-4 text-center">
             <Link to={ 'https://docs.google.com/forms/d/e/1FAIpQLSd5A-0NZCqNjvGZlLgk0BVDRULKV9hMT8UVZ7I_6MSbyd-Mtw/viewform' } 
                 target="_blank" className="font-semibold text-black">
-                Please signup for our next general membership meeting, Tuesday March 19, 2024 @ 6:00pm Via RingCentral
+                Please signup for our next general membership meeting, Tuesday May 21, 2024 @ 6:00pm Via RingCentral
             </Link>
         </div>
     );
