0424d82e0f1ccb55fbda0694ab30fa851aa58df9
Moi Hi <moises.h@outlook.com>
2025-02-05 15:10:45 -0500

    Scroll to top on nav and code clean up

--- src/components/scroll-to-top.js
+++ src/components/scroll-to-top.js
@@ -2,11 +2,14 @@ import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const ScrollToTop = () => {
-  const { pathName } = useLocation();
+  const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathName]);
+    requestAnimationFrame(() => {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+
+  }, [pathname]);
 
   return null;
 };
