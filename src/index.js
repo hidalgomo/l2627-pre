fb24bb636c059b0132b160123f6884fb4e4a77c5
hidalgomo <105666031+hidalgomo@users.noreply.github.com>
2025-02-05 14:14:54 -0500

    ScrollToTop added

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import AppRoutes from './app-routes';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <AppRoutes />
     </BrowserRouter>
   </React.StrictMode>
